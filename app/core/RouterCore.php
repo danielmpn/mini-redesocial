ba78037b4314149ca90f31387cb7609b9c081744
Daniel Matos Pantuffi <danielkld@hotmail.com>
2021-04-10 23:57:08 -0300

    att 10-04-21

--- app/core/RouterCore.php
+++ app/core/RouterCore.php
@@ -13,6 +13,9 @@ class RouterCore
     {
         $this->method = $_SERVER['REQUEST_METHOD'];
         $uri = $_SERVER['REQUEST_URI'];
+        $uri = substr($uri, 1);
+        $this->uri = explode("/", $uri);
 
+        echo $this->uri[1];
     }
 }
--- public/index.php
+++ public/index.php
@@ -2,7 +2,5 @@
 
 require_once './../app/config/config.php'; //configurações do app
 require_once './../vendor/autoload.php'; //autoload do vendor
-//$rota = new Rota;
 
-$teste = new TesteController();
 (new RouterCore);
